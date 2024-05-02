4db561dd34312a2d48c4e2a7b9b900ed281e5280
Nikhil2005Rai <n2005rai@gmail.com>
2024-05-02 21:59:29 +0530

    added about, contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 import './App.css'
 
 function App() {
@@ -7,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-green-600 p-4">React Router</h1>
+    
     </>
   );
 }
