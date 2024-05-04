64f9248b9a4b172253c74087b7cff9126d7b4529
Nikhil2005Rai <n2005rai@gmail.com>
2024-05-04 09:39:25 +0530

    additions

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const User = () => {
-    const {userid} = useParams()
-  return <div>User: {userid}</div>;
+  const { userid } = useParams();
+  return (
+    <div className="bg-gray-600 text-white text-3xl p-4">User: {userid}</div>
+  );
 };
 
 export default User;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
 import User from "./components/User/User.jsx";
+import Github from "./components/Github/Github.jsx";
 import App from "./App.jsx";
 import "./index.css";
 import {
@@ -42,6 +43,9 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
       <Route path="user/:userid" element={<User />} />
+      <Route 
+      path="github" 
+      element={<Github />} />
     </Route>
   )
 );
